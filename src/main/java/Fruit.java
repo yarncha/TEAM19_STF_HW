9112ced4e46a33e23aee785a911facea1433ad58
crystal <sjfs0709@gmail.com>
2019-04-29 21:43:46 +0900

    sujeong's commit

--- src/main/java/Fruit.java
+++ src/main/java/Fruit.java
@@ -8,4 +8,14 @@ public class Fruit {
     private int price;
     private int count;
     private String quality;
+
+    //가격 비교 메소드
+    public boolean comparePrice(int customerMoney){
+        if(customerMoney>=this.price){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
 }
